1b6e9befb804fba4b4a4176d25e89c82302fbed5
Abil Momin <abilmomin6@gmail.com>
2026-04-26 12:12:22 -0600

    added week2 component

---
+++ app/week-2/student.info.tsx
@@ -0,0 +1,10 @@
+export default function StudentInfo() {
+  return (
+    <div>
+      <h1>Abil Momin</h1>
+      <a href="https://github.com/abilmomin/cprg306-assignments">
+        https://github.com/abilmomin/cprg306-assignments
+      </a>
+    </div>
+  );
+}
